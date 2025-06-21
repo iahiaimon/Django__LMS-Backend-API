5dd128cde64cb3eeedf2d9c3b5bb9dd774be6347
iahia <iahiaimon555@gmail.com>
2025-06-22 00:59:10 +0600

    Creating Apps

--- LMS_Backend_API/settings.py
+++ LMS_Backend_API/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'accounts',
+    'categories',
+    'rest_framework',
+
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.models.base_model import BaseModel
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    class RoleChoices(models.TextChoices):
+        STUDENTS = "s" , "Student"
+        TEACHERS = "t" , "Teacher"
+        ADMIN = "a" , "Admin"
+    mobile = models.CharField(max_length=15, unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices = RoleChoices.choices,
+        default = RoleChoices.STUDENTS
+
+    )
